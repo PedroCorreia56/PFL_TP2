c1c2dcdc82d44f852b54f99102035520fc62f9b2
PedradasDaTuga <pedromiguelcorreia2002@gmail.com>
2022-12-31 18:04:20 +0000

    Reading last line works

--- logic.pl
+++ logic.pl
@@ -46,7 +46,7 @@ check_not_scared_elem(0,0,Board,PlayerTurn):-
         nth0(0,Line,Elem),
         check_elem(Elem,PlayerTurn),
         scared_of(Elem,Scared), 
-        ElemRightNumber=0+1,
+        ElemRightNumber is 0+1,
         nth0(ElemRightNumber,Line,Elemright),
         Elemright\=Scared,
         Elemright\=9+Scared,
@@ -135,69 +135,65 @@ check_not_scared_elem(0,9,Board,PlayerTurn):-
 % Case for elements between the first and the last of the last line
 check_not_scared_elem(ColumnNumber,9,Board,PlayerTurn):-
         nonvar(ColumnNumber),
-        write('Outros numeor'),nl,
-        write('ColumnNumber:'),
-        write(ColumnNumber),nl,
         ColumnNumber\=0,
         ColumnNumber\=9,
         nth0(9,Board,Line),
         nth0(ColumnNumber,Line,Elem),
-        write('Before check elem'),nl,
+    
         check_elem(Elem,PlayerTurn),
-        write('After check elem'),nl,
+      
         scared_of(Elem,Scared), 
         ElemRightNumber is ColumnNumber+1,      
         ElemLeftNumber is ColumnNumber-1,
         LineaboveNumber is 9-1,
-        write('In function check scared elem'),nl,
-        write('Elem:'),write(Elem),nl,
+     
+
         nth0(ElemRightNumber,Line,Elemright),
-        write('Elem righht:'),write(Elemright),nl,
+
         Elemright\=Scared,
         Elemright\=9+Scared,
         nth0(ElemLeftNumber,Line,Elemleft),
-         write('Elem left:'),write(Elemleft),nl,
+       
         Elemleft\=Scared,
         Elemleft\=9+Scared,
         nth0(LineaboveNumber,Board,Lineabove),
         nth0(ColumnNumber,Lineabove,Elemabove),
-         write('Elem above:'),write(Elemabove),nl,
+     
         Elemabove\=Scared,
         Elemabove\=9+Scared,
         nth0(ElemRightNumber,Lineabove,Elemrightdiagonal),
-         write('Elem right above:'),write(Elemrightdiagonal),nl,
+
         Elemrightdiagonal\=Scared,
         Elemrightdiagonal\=9+Scared,
         nth0(ElemLeftNumber,Lineabove,Elemleftdiagonal),
-            write('Elem left above:'),write(Elemleftdiagonal),nl,
+
         Elemleftdiagonal\=Scared,
-        Elemleftdiagonal\=9+Scared,
-        write('End of function check scared elem'),nl.
+        Elemleftdiagonal\=9+Scared.
+      
         
 % Case for the last element of the last line
 check_not_scared_elem(9,9,Board,PlayerTurn):-
-        write('Ultimo numero(9)'),nl,
+
         nth0(9,Board,Line),
         nth0(9,Line,Elem),
-         write('Before check elem'),nl,
+ 
         check_elem(Elem,PlayerTurn),
-          write('After check elem'),nl,
+ 
         scared_of(Elem,Scared), 
         ElemLeftNumber is 9-1,
         nth0(ElemLeftNumber,Line,Elemleft),
-         write('Elem left:'),write(Elemleft),nl,
+
         Elemleft\=Scared,
         Elemleft\=9+Scared,
         nth0(8,Board,Lineabove),
         nth0(9,Lineabove,Elemabove),
-        write('Elem above:'),write(Elemabove),nl,
+
         Elemabove\=Scared,
         Elemabove\=9+Scared,
         nth0(ElemLeftNumber,Lineabove,Elemleftdiagonal),
-               write('Elem left above:'),write(Elemleftdiagonal),nl,
+         
         Elemleftdiagonal\=Scared,
-        Elemleftdiagonal\=9+Scared,
-        write('Final').
+        Elemleftdiagonal\=9+Scared.
 
 
        % Next_Elem is 0+1,
@@ -332,42 +328,36 @@ check_not_scared_elem(ColumnNumber,LineNumayerTurn,Board,PlayerTurn):-
         write(ScaredPiece).
 */
 
-/*
-check_scared_line(9,9,[Board,PlayerTurn],ScaredPiece):-
-        \+check_not_scared_elem(9,9,Board,PlayerTurn), write('Depois de 99'),
-        var(ScaredPiece).
-*/
-/*check_scared_line(9,9,[Board,PlayerTurn],ScaredPiece):-
-        check_not_scared_elem(9,9,Board,PlayerTurn),
-        ScaredPiece=9-9,
-        write('Depois de 99'),
-        nonvar(ScaredPiece).
-*/
+check_scared_line(C,L,[Board,PlayerTurn],ScaredPiece):-
+        \+check_not_scared_elem(C,L,Board,PlayerTurn),
+        nth0(L,Board,Line),
+        nth0(C,Line,Elem),
+        check_elem(Elem,PlayerTurn),
+        ScaredPiece=L-C.
+
+check_scared_line(ElemNumber,LineNumber,[Board,PlayerTurn],ScaredPiece):-
+        ElemNumber\=9,
+        LineNumber\=10,
+        check_not_scared_elem(ElemNumber,LineNumber,Board,PlayerTurn),
+        var(ScaredPiece),
+        Next_Elem is ElemNumber+1,
+        check_scared_line(Next_Elem,LineNumber,[Board,PlayerTurn],ScaredPiece).
+
 check_scared_line(9,LineNumber,[Board,PlayerTurn],ScaredPiece):-
         LineNumber\=9,
         check_not_scared_elem(ElemNumber,LineNumber,Board,PlayerTurn),
         var(ScaredPiece),
         NextLie is LineNumber+1,
-        check_scared_line(0,NextLine,[Board,PlayerTurn],ScaredPiece).
-
-
-
-check_scared_line(ElemNumber,LineNumber,[Board,PlayerTurn],ScaredPiece):-
-        ElemNumber\=9,
-        LineNumber\=9,
-        write('Elem:'),write(ElemNumber),write('Line:'),write(LineNumber),nl,
-        check_not_scared_elem(ElemNumber,LineNumbeerTurn),
-        write('After func'),nl,
-        var(ScaredPiece),
-        Next_Elem is ElemNumber+1,
-        check_scared_line(Next_Elem,LineNumber,[Board,PlayerTurn],ScaredPiece).
+        check_scared_line(0,NextLie,[Board,PlayerTurn],ScaredPiece).
 
 check_scared_line(9,9,[Board,PlayerTurn],ScaredPiece):-
-        check_not_scared_elem(9,9,Board,PlayerTurn).
+        check_not_scared_elem(9,9,Board,PlayerTurn),
+        var(ScaredPiece).
 
-check_scared_line(C,L,[Board,PlayerTurn],ScaredPiece):-
-        \+check_not_scared_elem(C,L,Board,PlayerTurn),
-        ScaredPiece=L-C.
+
+
+
+
 
 % check_scared(+GameState,+LineToCheck,-ScaredPiece)
 % check if any piece is near a piec they are scared of
@@ -383,4 +373,10 @@ testee:-
 
 teste2:-   
         initial(1,[Board,PlayerTurn]),
-        check_scared_line(9,9,[Board,PlayerTurn],ScaredPiece),write(ScaredPiece),nl.
+        check_scared_line(0,0,[Board,PlayerTurn],ScaredPiece),
+        var(ScaredPiece).
+
+teste3:-
+        initial(1,[Board,PlayerTurn]),
+        check_scared_line(0,0,[Board,PlayerTurn],ScaredPiece),
+        nonvar(ScaredPiece),nl,write(ScaredPiece),nl.
